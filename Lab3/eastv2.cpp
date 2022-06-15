d5ead33d2cd23199d1461597703d789431d717ea
Fullooh <angstdemon1@aol.com>
2022-06-15 01:54:39 -0400

    Update to repo

---
+++ Lab3/eastv2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <climits>
+
+using namespace std;
+
+
+int main()
+{
+ifstream fin("Current_Reservoir_Levels.tsv");
+if (fin.fail()) {
+    cerr << "File cannot be opened for reading." << endl;
+    exit(1); // exit if failed to open the file
+}
+
+string junk;        // new string variable
+getline(fin, junk); // read one line from the file
+
+string date, input;
+double eastSt, eastEl, westSt, westEl;
+
+cout << "Enter Date: ";
+cin >> input;
+
+while(fin >> date >> eastSt >> eastEl >> westSt >> westEl) { 
+    // this loop reads the file line-by-line
+    // extracting 5 values on each iteration 
+    
+    fin.ignore(INT_MAX, '\n'); //skips to the end of line, 
+                          //ignorring the remaining columns 
+ 
+    // OUR CODE GOES HERE
+    if (input == date){
+    cout << "East basin storage: " << eastSt << " billion gallons" << endl;
+    }
+}
+
+fin.close();
+
+}
