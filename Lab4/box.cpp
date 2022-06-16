8031680b8eb042fe56dffd5107769fe4fb3be520
Fullooh <angstdemon1@aol.com>
2022-06-16 02:13:37 -0400

    Update to repo

--- Lab4/box.cpp
+++ Lab4/box.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main()
 {
+    int height = 0;
+    int width = 0;
+
+    cout << "Input Width: ";
+    cin >> width;
+
+    cout << "Input Height: ";
+    cin >> height;
+
     
 
 
