a7419e98368bcdb519367e675accdf40791d93d8
Fullooh <angstdemon1@aol.com>
2022-06-16 03:18:48 -0400

    Update to repo

--- Homework/E3.1.cpp
+++ Homework/E3.1.cpp
@@ -4,7 +4,20 @@ using namespace std;
 
 int main()
 {
+    int i;
     
+    cout << "Enter number: ";
+    cin >> i;
+
+    if(i == 0){
+        cout << "Zero" << endl;
+    } 
+    else if(i > 0){
+        cout << "Positive Number" << endl;
+    }
+    else{
+        cout << "Negative Number" << endl;
+    }
 
 
 }
