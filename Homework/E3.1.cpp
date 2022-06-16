a401033d3a55f69fe67ed74825c88683e72aaa06
Fullooh <angstdemon1@aol.com>
2022-06-16 02:07:21 -0400

    Update to repo

---
+++ Homework/E3.1.cpp
@@ -0,0 +1,26 @@
+//
+//
+//
+//
+//
+//
+//
+
+#include <iostream>
+#include <string.h>
+
+
+using namespace std;
+
+int main()
+{
+    char word[20];
+
+    cout << "Enter word: ";
+    cin.get (word, 20, '\n');
+
+    for (int i = 0; i < word[i] != '\0'; i++)
+    cout << word[i] << endl;
+
+
+}
---
+++ Homework/E3.1v2.cpp
@@ -0,0 +1,26 @@
+//
+//
+//
+//
+//
+//
+//
+
+#include <iostream>
+#include <string.h>
+
+
+using namespace std;
+
+int main()
+{
+    char word;
+
+    cout << "Enter word: ";
+    cin >> word, '\n';
+
+    for (int i = 0; i < word != '\0'; i++)
+    cout << word << endl;
+    
+
+}
