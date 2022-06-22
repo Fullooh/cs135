4f663bdc2819eeb5c52cebd244082b56a7f360ae
Fullooh <angstdemon1@aol.com>
2022-06-22 13:00:53 -0400

    Update to repo

---
+++ Lab4/checkerboard.cpp
@@ -0,0 +1,33 @@
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int width;
+    int height;
+    int i;
+    int j;
+
+    cout << "Input Width: ";
+    cin >> width;
+
+    cout << "Input Height: ";
+    cin >> height;
+
+    cout << "Shape:" << endl;
+
+    
+
+
+}
