8196299037e0b494115cdb8575fce2747ccefe09
Fullooh <angstdemon1@aol.com>
2022-06-22 15:56:05 -0400

    Update to repo

---
+++ Lab4/lower.cpp
@@ -0,0 +1,36 @@
+//Angst S Gregory
+//
+//
+//
+//
+//
+//
+//
+//
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{  
+    int j, i, size;
+    cout << "Input Size Length: ";
+    cin >>  size;
+    cout << "Shape:"  << endl;
+
+    for( i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+            if (i >= j)
+            {
+                cout << "*";
+            }
+            else 
+            {
+                cout << " ";
+            }
+        }
+         cout << endl;
+    }
+}
