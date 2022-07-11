b1593262284a307b01e2f8920b3c0d53978612b7
Fullooh <angstdemon1@aol.com>
2022-07-11 15:43:03 -0400

    Update to repo

--- Lab6/test-ascii.cpp
+++ Lab6/test-ascii.cpp
@@ -1,2 +1,17 @@
+#include <iostream>
 
+using namespace std;
 
+int main ()
+{
+    string input;
+    char c;
+
+    cout << "Input: ";
+    getline(cin, input);
+
+    for (int i = 0; i < input.length(); i++)
+    {
+        cout << input[i] << " " << int(c) << endl;
+    }
+}
