5ff5565088596c18395d1e0de4adb43a3dd36ee6
Fullooh <angstdemon1@aol.com>
2022-07-11 14:53:35 -0400

    Update to repo

--- Lab6/test-ascii.cpp
+++ Lab6/test-ascii.cpp
@@ -1,21 +1,2 @@
-//
-//
-//
-//
-//
-//
-//
-//
-//
 
-
-#include <iostream>
-#include <cassert>
-#include <cstdlib>
-#include <fstream>
-
-using namespace std;
-
-void readImage(int image[MAX_H][MAX_W], int &height, int &width);
-    
 
