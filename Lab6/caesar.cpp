029ffd3748d20292ec53c9b78116e48d290b3560
Fullooh <angstdemon1@aol.com>
2022-07-16 22:01:52 -0400

    Update to repo

--- Lab6/caesar.cpp
+++ Lab6/caesar.cpp
@@ -14,8 +14,22 @@
 
 using namespace std;
 
-int main ()
+char shiftChar(char c, int rshift)
 {
-    
+
+
+if ((int) c >= 65 && (int) c <= 90)
+{
+    toupper(c);
+    return c;
+}
+if ((int) c >= 97 && (int) c <= 122)
+{
+    return c;
+}
+else
+{
+    return c;
+}
 
 }
