e4a68a6a31dd1216c143c7745bae459fbcfc1eca
Fullooh <angstdemon1@aol.com>
2022-07-18 00:19:35 -0400

    Update to repo

--- Projects/calc.cpp
+++ Projects/calc.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main()
 {
-    
+    int num1, num2;
+    char op;
+
+    cout << "Enter First Integer: ";
+    cin >> num1;
+
+    cout << "Enter Operation: ";
+    cin >> op;
+
+    cout << "Enter Second Integer: ";
+    cin >> num2;
 
 }
